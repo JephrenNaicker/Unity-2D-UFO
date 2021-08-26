cf2ae5d15c2d2ee09b5cf9a80e47a136613fc5ce
Jephren <54954891+JephrenNaicker@users.noreply.github.com>
2021-08-26 20:14:41 +0200

    New Scene and Asteriod  behavior update
    Added Bounce of wall script and lv 3

--- Assets/Scripts/AsteroidController.cs
+++ Assets/Scripts/AsteroidController.cs
@@ -5,54 +5,56 @@ using UnityEngine;
 public class AsteroidController : MonoBehaviour
 {
 
+    [SerializeField]
+    
+    private Vector3 initialVelocity;
+
+    [SerializeField]
+    [Tooltip("Velocity on Bounce")]
+    private float minVelocity = 6f;
+
+    private Vector3 lastFrameVelocity;
 
     Rigidbody2D m_Rigidbody;
 
     [SerializeField]
-        public float m_Speed;    
-    
+    [Tooltip("Speed on Start")]
+    public float m_Speed;    
     
    private void Awake() 
    {
         m_Rigidbody = GetComponent<Rigidbody2D>();
-        m_Speed=250f;    
-
+        m_Speed=300f;    
+        m_Rigidbody.velocity = initialVelocity;
    }
 
 
   private  void Start()
     {
-
+       int RandomRotation = Random.Range(-45,45);
+         transform.Rotate(new Vector3(0,0, RandomRotation)*Time.deltaTime);
         addStartForce();
     }
 
     // Update is called once per frame
     void Update()
     {
-
-         transform.Rotate(new Vector3(0,0,-45)*Time.deltaTime);
-
+        lastFrameVelocity=m_Rigidbody.velocity;
     }
 
 
 
-    //  private void OnCollisionEnter2D(Collision2D other) 
-    //  {
+     private void OnCollisionEnter2D(Collision2D other) 
+     {
     
-    //     if(other.gameObject.CompareTag("Wall"))
-    //     {
+        // if(other.gameObject.CompareTag("Wall"))
+        // {
           
-    //       Debug.Log("can see wall");
-    //     //   transform.Rotate(new Vector3(0,0,55)*Time.deltaTime);
-    //     //   Debug.Log("Rotate");
-    //     //    transform.Translate(Vector3.left * Time.deltaTime);
+         // Debug.Log("can see wall");
+         Bounce(other.contacts[0].normal);
 
-    //        //m_Rigidbody.velocity = transform.forward * m_Speed;
-    //        addStartForce();
-    //        //transform.Rotate(Vector3.left * Time.deltaTime);
-
-    //     }
-    // }
+        //}
+    }
 
 
     private void addStartForce()
@@ -66,6 +68,15 @@ public class AsteroidController : MonoBehaviour
         m_Rigidbody.AddForce(ChangeDirection * m_Speed);
 
 
+    }
+
+   private void Bounce(Vector3 collisionNormal)
+    {
+        var speed = lastFrameVelocity.magnitude;
+        var direction = Vector3.Reflect(lastFrameVelocity.normalized, collisionNormal);
+
+        //Debug.Log("Out Direction: " + direction);
+        m_Rigidbody.velocity = direction * Mathf.Max(speed, minVelocity);
     }
 
 
