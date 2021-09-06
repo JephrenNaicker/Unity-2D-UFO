71480371edf81d5a7336fda54421c3379dd352c4
Jephren <54954891+JephrenNaicker@users.noreply.github.com>
2021-09-06 16:25:13 +0200

    Added Health/damage Bar
    Added Health/damage Bar , and HUD changed score to Collect

---
+++ Assets/Scripts/HeathBarController.cs
@@ -0,0 +1,24 @@
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HeathBarController : MonoBehaviour
+{
+
+
+    public Slider slider;
+
+
+public void SetMaxHealth(float health)
+{
+ slider.maxValue = health;
+  slider.value = health;
+}
+
+   public void setHealth(float health)
+   {
+       slider.value = health;
+   }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,8 @@
-
+//using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
@@ -14,12 +18,14 @@ public class PlayerController : MonoBehaviour
     public float speed;
     public Text txtCounter;
     public Text txtWintext;
-
+    public Text txtDamage;
     public AudioSource PickupAudio;
 
     public AudioSource PortalActiveAudio;
 
     public AudioSource UFOMoveAudio;
+    
+    public HeathBarController healthBar;
 
   // public ParticleSystem AstroHit;
     public GameObject portal;
@@ -34,9 +40,13 @@ public class PlayerController : MonoBehaviour
        rb2d=GetComponent<Rigidbody2D>();
        countScore = 0;
        Health=0f;
+      
        MaxHealth=100f;
-       AsteroidBadDamage=0.25f;
+       healthBar.SetMaxHealth(1f);
+       healthBar.setHealth(0f);
+       AsteroidBadDamage=UnityEngine.Random.Range (0.20f,0.25f);
        txtWintext.text="";
+       txtDamage.text="";
        CounterUpdate();
         /*Set the UFO Shader*/
        UFOInvertColour.SetFloat("_Threshold",0);
@@ -62,7 +72,18 @@ public class PlayerController : MonoBehaviour
 
 
 private void Update() {
-       if(rb2d.velocity.magnitude > 8)
+   IsUFOMoving();
+  
+}
+
+
+
+/*
+*Plays UFOMoveAudio when UFO is Moving
+*/
+void IsUFOMoving()
+{
+    if(rb2d.velocity.magnitude > 8)
       {      
           isMoving= true; 
       }else 
@@ -83,17 +104,17 @@ private void Update() {
         
           UFOMoveAudio.Stop();
       }
-  
 }
 
-
+/*
+*Updates Score,after DataPickUp
+*/
    void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.CompareTag("PickUp"))
         {
-            other.gameObject.SetActive(false);
-            PickupAudio.Play();
             Destroy(other.gameObject);
+            PickupAudio.Play();
             countScore++;
             CounterUpdate();
            
@@ -112,23 +133,34 @@ private void Update() {
           UFOInvertColour.SetFloat("_Threshold",UFOLife());
  
           //AstroHit.Play();
-        
+           if(Health>=1f)
+           {
+             Debug.Log("UFO Crashed");
+             Application.Quit();  
+           }
 
         }
     }
 
-
+/*
+*Bug:player Still takes damage after level Clear
+*/
 float UFOLife()
 {
-    
-   return Health=Health+AsteroidBadDamage;
+    var CurrHealth = (Health+=AsteroidBadDamage);
+    UnityEngine.Debug.Log("CurrHealth"+CurrHealth);
+    healthBar.setHealth(CurrHealth);
+    txtDamage.text=CurrHealth.ToString();
+   return CurrHealth;
 }
 
 
-
+   /*
+   *Tracks Score for different Levels
+   */
     void CounterUpdate()
     {
-      txtCounter.text="Score:"+countScore.ToString();
+      txtCounter.text="Collect:"+countScore.ToString();
       if(countScore>=6&&GetCurrentActiveScene()==1){
 
           txtWintext.text = "Level Clear";
@@ -148,10 +180,14 @@ float UFOLife()
        else if(countScore>=14&&GetCurrentActiveScene()==3)
        {
             txtWintext.text = "You WIN";
+            //Time.timeScale= 0f;//pause or freeze all objects within a scene *BUG Song still plays
+           // UFOMoveAudio.Stop();
        }
     }
 
-     
+     /*
+     Get The Current ActiveScene 
+     */
     public int  GetCurrentActiveScene()
     {
       return    SceneManager.GetActiveScene().buildIndex;
